760c791445df2f92ab04d3e38744149e43307628
jakobboedker <bdkerjakob1@gmail.com>
2024-08-27 13:35:38 +0200

    making the server and functions that will execute on local server

---
+++ server.js
@@ -0,0 +1,71 @@
+const puppeteer = require('puppeteer');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+port = 3000
+const password = 'mitpassword'
+const user = 'xjbha@evida.dk'
+
+//function that opens TDC selvbetjening and login
+async function openTDC() {
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.goto('https://selvbetjening.tdc.dk/');
+    await page.waitForSelector('#username');
+    await page.type('#username', user);
+    await page.type('#password', password);
+    await page.click('#loginButton');
+    await page.waitForSelector('#ink');
+    const ink = await page.evaluate(() => {
+        return document.querySelector('#ink').innerText;
+    });
+    console.log(ink);
+    await browser.close();
+    }
+
+
+//get request that opens TDC selvbetjening and login
+app.get('/tdc-login', async (req, res) => {
+  try {
+        openTDC();
+        res.send('TDC opened');
+    } catch (error) {
+        console.log(error);
+        res.send('Something went wrong');
+    }
+});
+
+app.get('/tdc-oprettelse', async (req, res) => {
+    try{
+        console.log('test')
+        res.send('test worked')
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.get('/tdc-opsigelse', async (req, res) => {
+    try{
+        console.log('test')
+        res.send('test worked')
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.get('/tdc-nummerflytning', async (req, res) => {
+    try{
+        console.log('test')
+        res.send('test worked')
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+
+//listen on port 3000
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
