def2d8b7f7b338059b3e457804627157faa68734
jakobboedker <bdkerjakob1@gmail.com>
2024-08-30 14:09:21 +0200

    split up, api and puppeteer automation

--- server.js
+++ server.js
@@ -1,86 +1,18 @@
-const puppeteer = require('puppeteer');
 const express = require('express');
 const app = express();
+const {oprettelse} = require('automation.js');
+const port = 5000
 
+app.use(express.json())
 
-port = 3000
-const password = '***********'
-const user = '************'
-
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-//function that opens TDC selvbetjening and login
-
-async function oprettelse(nummer, mail, navn) {
-    return 'test'
-}
-async function opsigelse(nummer, mail, navn) {
-    return 'test'
-}
-
-async function nummerflytning(nummer, mail, navn) {
-    return 'test'
-}
 
 //get request that opens TDC selvbetjening and login
 app.get('/tdc-login', async (req, res) => {
-  try {
-        const browser = await puppeteer.launch(
-            {headless: false},
-            {args: ['--incognito']}
-        );
-        const page = await browser.newPage();
-        await page.goto('https://selvbetjening.tdc.dk/');
-        await page.waitForSelector('#username');
-        await page.type('#username', user);
-        await page.type('#password', password);
-        await sleep(2000);
-        await page.click('#declineButton');
-        await page.click('#post-button');
-        //const ink = await page.evaluate(() => {
-        //    return document.querySelector('#ink').innerText;
-        //});
-        //console.log(ink);
-        await browser.close();
-        res.send('TDC opened');
-    } catch (error) {
-        console.log(error);
-        res.send('Something went wrong');
-    }
+    oprettelse();
+    res.send('TDC login')
 });
-
-app.get('/tdc-oprettelse', async (req, res) => {
-    try{
-        console.log('test')
-        res.send('test worked')
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-app.get('/tdc-opsigelse', async (req, res) => {
-    try{
-        console.log('test')
-        res.send('test worked')
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-app.get('/tdc-nummerflytning', async (req, res) => {
-    try{
-        console.log('test')
-        res.send('test worked')
-    } catch (error) {
-        console.log(error)
-    }
-})
-
 
 //listen on port 3000
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`server running on port ${port}`)
 })
